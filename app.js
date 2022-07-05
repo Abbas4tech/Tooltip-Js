4b443deffaf2ed653d376c1c5b0cccf7879ef03a
Abbas <ashaikh2@ischoolconnect.com>
2022-07-05 19:17:32 +0530

    Removed tooltip after hover and correct position

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 console.log("App is Running!");
 
+const removeAllTooltips = () =>
+  document.querySelectorAll(".tooltip").forEach((e) => e.remove());
+
 const tooltips = document.querySelectorAll("[data-tooltip-text]");
 console.log(tooltips);
 tooltips.forEach((tooltip) => {
+  tooltip.addEventListener("mouseout", () => removeAllTooltips());
   tooltip.addEventListener("mouseover", (e) => {
     const tooltipBox = document.createElement("div");
     tooltipBox.classList.add("tooltip");
     const tooltipText = e.currentTarget.getAttribute("data-tooltip-text");
     tooltipBox.textContent = tooltipText;
-    document.querySelectorAll(".tooltip").forEach((e) => e.remove());
+    removeAllTooltips();
     const cords = e.currentTarget.getBoundingClientRect();
-    console.log(cords.left);
+    tooltipBox.style.left = `${cords.left + e.currentTarget.clientWidth + 5}px`;
+    tooltipBox.style.top = `${cords.top}px`;
     document.body.append(tooltipBox);
   });
 });
