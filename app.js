3f00350273f01d3089566d3b58426f67232a370d
Abbas <ashaikh2@ischoolconnect.com>
2022-07-05 18:44:34 +0530

    Added basic mouseover event

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("App is Running!");
+
+const tooltips = document.querySelectorAll(".tooltip");
+console.log(tooltips);
+tooltips.forEach((tooltip) => {
+  tooltip.addEventListener("mouseover", () =>
+    console.log("You are hovered over me!")
+  );
+});
