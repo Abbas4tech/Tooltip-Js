64057223d964ecca42982567802814828de682ce
Abbas <ashaikh2@ischoolconnect.com>
2022-07-05 19:03:50 +0530

    Added single tooltip at a time

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 console.log("App is Running!");
 
-const tooltips = document.querySelectorAll(".tooltip");
+const tooltips = document.querySelectorAll("[data-tooltip-text]");
 console.log(tooltips);
 tooltips.forEach((tooltip) => {
-  tooltip.addEventListener("mouseover", () =>
-    console.log("You are hovered over me!")
-  );
+  tooltip.addEventListener("mouseover", (e) => {
+    const tooltipBox = document.createElement("div");
+    tooltipBox.classList.add("tooltip");
+    const tooltipText = e.currentTarget.getAttribute("data-tooltip-text");
+    tooltipBox.textContent = tooltipText;
+    document.querySelectorAll(".tooltip").forEach((e) => e.remove());
+    document.body.append(tooltipBox);
+  });
 });
